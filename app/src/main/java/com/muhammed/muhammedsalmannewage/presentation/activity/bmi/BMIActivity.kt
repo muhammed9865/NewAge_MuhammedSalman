c96c86f089648c657cf95192bc0b6a365772bc00
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-12-30 22:46:22 +0200

    add: setup Navigation graph and NavController

---
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/activity/bmi/BMIActivity.kt
@@ -0,0 +1,21 @@
+package com.muhammed.muhammedsalmannewage.presentation.activity.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.muhammed.muhammedsalmannewage.R
+
+class BMIActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navHostFragment.navController
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
