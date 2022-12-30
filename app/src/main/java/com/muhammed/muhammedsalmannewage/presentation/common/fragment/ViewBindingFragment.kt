343b6f3fac840d40d43aaf41673533158e185cc7
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-12-30 22:36:27 +0200

    add: ViewBindingFragment.kt to extract initializing ViewBinding on each fragment

---
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/common/fragment/ViewBindingFragment.kt
@@ -0,0 +1,31 @@
+package com.muhammed.muhammedsalmannewage.presentation.common.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+// Used to extract to routine of initializing the binding on each fragment
+// And destroy it onFragmentDestroy
+abstract class ViewBindingFragment <T : ViewBinding> : Fragment() {
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = initBinding()
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    abstract fun initBinding(): T
+}
