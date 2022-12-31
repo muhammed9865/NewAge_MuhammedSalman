a3e29eaf1d4718cd52039aff257631da4ba153b1
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-12-31 15:26:22 +0200

    setup Activity Toolbar with navigation component

--- app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/activity/bmi/BMIActivity.kt
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/activity/bmi/BMIActivity.kt
@@ -1,23 +1,78 @@
 package com.muhammed.muhammedsalmannewage.presentation.activity.bmi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.muhammed.muhammedsalmannewage.R
+import com.muhammed.muhammedsalmannewage.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class BMIActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     private val navController: NavController by lazy {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navHostFragment.navController
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        showSplashFor(SPLASH_DISPLAY_TIME)
+        setupToolbarWithNavigation()
+
+    }
+
+
+    private fun showSplashFor(milli: Long) {
+        with(binding) {
+            lifecycleScope.launch {
+                // Display the splash drawable manually if Build version is below Android 12
+                // Android 12+ displays a customizable splash screen.
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+                    toggleSplashVisibility(show = true)
+                    delay(milli)
+                    toggleSplashVisibility(show = false) {
+                        navHostFragmentContainer.visibility = View.VISIBLE
+                    }
+                } else {
+                    navHostFragmentContainer.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun toggleSplashVisibility(show: Boolean, onFinish: () -> Unit = {}) {
+        val scale = if (show) 1f else 0f
+        val alpha = if (show) 1f else 0f
+        binding.splashImage.animate()
+            .scaleX(scale)
+            .scaleY(scale)
+            .alpha(alpha)
+            .setDuration(500)
+            .withEndAction(onFinish)
+            .start()
+    }
+
+
+    private fun setupToolbarWithNavigation() {
+        NavigationUI.setupWithNavController(
+            binding.bmiToolbar,
+            navController
+        )
+    }
+
+
+    companion object {
+        private const val SPLASH_DISPLAY_TIME = 1000L
     }
 }
--- app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/common/fragment/ViewBindingFragment.kt
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/common/fragment/ViewBindingFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import dagger.hilt.android.AndroidEntryPoint
 
 // Used to extract to routine of initializing the binding on each fragment
 // And destroy it onFragmentDestroy
