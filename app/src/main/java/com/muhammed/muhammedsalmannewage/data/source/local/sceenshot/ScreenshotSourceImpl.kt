de8384ff04dc105cf869869f2dfb092fa65d3631
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2023-01-03 02:56:01 +0200

    add: ScreenshotSourceImpl.kt to save screenshots in files

--- app/src/main/java/com/muhammed/muhammedsalmannewage/data/source/local/sceenshot/ScreenshotSourceImpl.kt
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/data/source/local/sceenshot/ScreenshotSourceImpl.kt
@@ -1,8 +1,10 @@
 package com.muhammed.muhammedsalmannewage.data.source.local.sceenshot
 
+import com.muhammed.muhammedsalmannewage.domain.DomainConstants
 import com.muhammed.muhammedsalmannewage.domain.model.State
 import com.muhammed.muhammedsalmannewage.domain.model.screenshot.ScreenshotRequest
 import com.muhammed.muhammedsalmannewage.domain.model.screenshot.ScreenshotResponse
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -13,17 +15,28 @@ class ScreenshotSourceImpl @Inject constructor() : ScreenshotSource {
     @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun saveScreenshot(screenshotRequest: ScreenshotRequest): State<ScreenshotResponse> {
         // Preparing file
-        val fileLocation = screenshotRequest.saveLocation + File.separator + "temp_screenshot.jpeg"
-        val file = File(fileLocation)
+        val fileLocation = screenshotRequest.saveLocation + File.separator
+        val screenshotsDir = File(fileLocation)
+
+        if (!screenshotsDir.exists())
+            screenshotsDir.mkdir()
+
+        val imageFile = File.createTempFile(System.currentTimeMillis().toString(),
+            SCREENSHOT_NAME,
+            screenshotsDir)
 
         // Trying to save the file and return it's path
         return try {
-            file.createNewFile()
-            val fo = FileOutputStream(file)
+            val fo = FileOutputStream(imageFile)
             fo.write(screenshotRequest.bytes.toByteArray())
+            fo.close()
+
+            // Deletes the file after some time
+            deleteImageAfterTime(imageFile)
+
             State.Success(
                 data = ScreenshotResponse(
-                    screenshotPath = fileLocation
+                    screenshotPath = imageFile.absolutePath
                 )
             )
         } catch (e: IOException) {
@@ -34,5 +47,20 @@ class ScreenshotSourceImpl @Inject constructor() : ScreenshotSource {
                 throwable = e
             )
         }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun deleteImageAfterTime(file: File) {
+        // Using the GlobalScope to make sure that it's not cancelled when the calling coroutine is cancelled
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(DomainConstants.SCREENSHOT_DELETE_DELAY)
+            file.delete()
+            cancel()
+        }
+    }
+
+    companion object {
+
+        private const val SCREENSHOT_NAME = "screenshot${DomainConstants.SCREENSHOT_EXT}"
     }
 }
---
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/domain/DomainConstants.kt
@@ -0,0 +1,6 @@
+package com.muhammed.muhammedsalmannewage.domain
+
+object DomainConstants {
+    const val SCREENSHOT_EXT = ".png"
+    const val SCREENSHOT_DELETE_DELAY = 20000L
+}
