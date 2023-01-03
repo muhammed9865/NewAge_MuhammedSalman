48c0bbf99c49da38fef289dc8b64939e528ceab7
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2023-01-04 00:21:30 +0200

    refactor: extract the metrics default values to be determined by domain layer @DomainConstants.kt

--- app/src/main/java/com/muhammed/muhammedsalmannewage/domain/DomainConstants.kt
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/domain/DomainConstants.kt
@@ -1,6 +1,14 @@
 package com.muhammed.muhammedsalmannewage.domain
 
+import com.muhammed.muhammedsalmannewage.domain.model.bmi.Gender
+
 object DomainConstants {
     const val SCREENSHOT_EXT = ".png"
     const val SCREENSHOT_DELETE_DELAY = 20000L
+
+    // Used for the BMI calculator as the default values of metrics
+    const val DEFAULT_SELECTED_WEIGHT = 43
+    const val DEFAULT_SELECTED_HEIGHT = 127
+    val DEFAULT_SELECTED_GENDER = Gender.FEMALE
+
 }
--- app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/activity/bmi/fragment/calculator/CalculatorUiState.kt
+++ app/src/main/java/com/muhammed/muhammedsalmannewage/presentation/activity/bmi/fragment/calculator/CalculatorUiState.kt
@@ -1,5 +1,6 @@
 package com.muhammed.muhammedsalmannewage.presentation.activity.bmi.fragment.calculator
 
+import com.muhammed.muhammedsalmannewage.domain.DomainConstants
 import com.muhammed.muhammedsalmannewage.domain.model.bmi.BMIResult
 import com.muhammed.muhammedsalmannewage.domain.model.bmi.Gender
 /*
@@ -12,15 +13,12 @@ import com.muhammed.muhammedsalmannewage.domain.model.bmi.Gender
  */
 data class CalculatorUiState(
     val showAd: Boolean = false,
-    val selectedWeight: Int = DEFAULT_WEIGHT,
-    val selectedHeight: Int = DEFAULT_HEIGHT,
-    val selectedGender: Gender = Gender.MALE,
+    val selectedWeight: Int = DomainConstants.DEFAULT_SELECTED_WEIGHT,
+    val selectedHeight: Int = DomainConstants.DEFAULT_SELECTED_HEIGHT,
+    val selectedGender: Gender = DomainConstants.DEFAULT_SELECTED_GENDER,
     val bmiResult: BMIResult? = null,
     val weightsList: List<Int> = emptyList(),
     val heightsList: List<Int> = emptyList(),
     val genderList: List<Gender> = emptyList()
 )
-
-private const val DEFAULT_WEIGHT = 43
-private const val DEFAULT_HEIGHT = 127
 
